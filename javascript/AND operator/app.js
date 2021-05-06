3ed6919d3a0763adf36bd411cd2cead8042342ba
Mariam-Saddiqa <mariam_saddiqa@yahoo.com>
2021-05-06 15:48:50 +0200

    update

---
+++ javascript/AND operator/app.js
@@ -0,0 +1,15 @@
+let objDataType = {
+    
+    name: 'Mariam',
+    LastName: 'Saddiqa',
+    age: '29',
+    country:'Pakistan',
+education:{
+    school: 'NJA',
+    collage: 'NJC',
+    bachelors: 'university Sargodha'
+}
+}
+
+let result="My name is " + objDataType.name + objDataType.LastName + ". I am "+objDataType.age+" old."
+console.log(result);
