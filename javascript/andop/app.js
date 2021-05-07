2095d635f680050f8c0c676fd59f8c60ec74fc86
Mariam-Saddiqa <mariam_saddiqa@yahoo.com>
2021-05-07 10:33:27 +0200

    update

--- javascript/andop/app.js
+++ javascript/andop/app.js
@@ -1,17 +1,49 @@
 let objDataType = {
-    
-    name: 'Mariam',
-    LastName: 'Saddiqa',
-    age: '29',
-    country:'Pakistan',
-education:{
-    school: 'NJA',
-    collage: 'NJC',
-    bachelors: 'university Sargodha'
-}
-}
+  name: "Mariam",
+  LastName: "Saddiqa",
+  age: "29",
+  country: "Pakistan",
+  education: {
+    school: "NJA",
+    collage: "NJC",
+    bachelors: "university Sargodha",
+  },
+  hobbies: {
+    activity: ["drawing", "reading", "movies"],
+    books: ["comic", "romantic", "fictional"],
+    husband: {
+      name: "Umair",
+      age: "29",
+      children: {
+        child1: {
+          name: "Ayra",
+          age: "4",
+        },
+        child2: {
+          name: "Zainab",
+          age: "4",
+        },
+      },
+    },
+  },
+};
 
-let result="My name is " + objDataType.name + objDataType.LastName + ". I am "+objDataType.age+" old."+ "I am from "+objDataType.country
+let result =
+  "My name is " +
+  objDataType.name +
+  objDataType.LastName +
+  ". I am " +
+  objDataType.age +
+  " old." +
+  "I am from " +
+  objDataType.country;
 console.log(result);
-let result2 = "I went to "+objDataType.education.school+". My college was "+objDataType.education.collage+". I did bachelors from "+objDataType.education.bachelors+"."
+let result2 =
+  "I went to " +
+  objDataType.education.school +
+  ". My college was " +
+  objDataType.education.collage +
+  ". I did bachelors from " +
+  objDataType.education.bachelors +
+  ".";
 console.log(result2);
