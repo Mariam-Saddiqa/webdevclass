8fe3613b0ceeb31981ddc1ff65ed3430188b8ebb
Mariam-Saddiqa <Mariam.Saddiqa@student.intecbrussel.be>
2021-05-18 09:28:28 +0200

    update

--- javascript/app.js
+++ javascript/app.js
@@ -1,5 +1,25 @@
-var distance = 200 ;
-var time = 4 ;
-var speed;
-var speed = distance/time;
-console.log(speed);
+let number1 = 20;
+let number2 = 25;
+let result;
+result = number1 == number2;
+
+let i;
+
+let str= 1
+let str1 = "1"
+let i = 5;
+for (let i = 0; i < 10; i++) {
+  // some statements
+}
+// 1. equal (values) = false
+// 2. data type not equal > true
+
+
+result = str !==str1
+
+
+
+
+
+console.log(result);
+console.log(i);
