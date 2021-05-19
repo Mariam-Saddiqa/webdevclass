d5a8069fb13bda63f6895f1fe4f8547ea50a0e0d
Mariam-Saddiqa <mariam_saddiqa@yahoo.com>
2021-05-19 15:05:55 +0200

    update

--- javascript/arraymethods/mapfilter.js
+++ javascript/arraymethods/mapfilter.js
@@ -1,25 +1,32 @@
 let student = [
   {
     name: "Ayesha",
-    score: 10,
+    score:[10,10,8,6,5]
   },
   {
     name: "Ayera",
-    score: 8,
+    score: [8,5,3,9,5]
   },
   {
     name: "Ali",
-    score: 7,
+    score: [7,10,9,8,4]
   },
 
   { name: "Hamza",
-    score: 9
+    score: [9,10,10,4,5]
   },
   {
     name: "Ahmed",
-    score: 3,
+    score: [3,5,6,8,5]
   },
 ];
+student.reduce(function(total,current){
+    
+   let newarr =console.log(`TOtal:${total.score}\nCurrent:${current.score}`);
+    
+    return total.score=+ current.score;
+})
+
 result= student.filter(function(x){
     return x.score>=8
 })
