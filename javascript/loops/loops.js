53f41ffa6157d59c900ac0ebab7a4103b6f98fbb
Mariam-Saddiqa <mariam_saddiqa@yahoo.com>
2021-05-28 10:17:15 +0200

    updated

---
+++ javascript/loops/loops.js
@@ -0,0 +1,13 @@
+let student=[
+    {name:'mariam',
+     age:29},
+     {name:'Barsam',
+     age:30},
+     {name:'lalena',
+     age:25},
+     {name:'yasmine',
+     age:28}]
+
+     for(student of student){
+         console.log(student)
+     }
