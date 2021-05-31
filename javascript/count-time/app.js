4c1bdedfd8df1269527f760edd7d4d7e00cf4fc7
Mariam-Saddiqa <mariam_saddiqa@yahoo.com>
2021-05-31 15:57:43 +0200

    update

--- javascript/count-time/app.js
+++ javascript/count-time/app.js
@@ -1,24 +1,19 @@
-
-let new_date=new Date('June 2, 2021').getTime;
-
+let new_date = new Date('2021-06-02');
 
 console.log(new_date);
 
+let count=document.getElementById("time1")
+setInterval(function () {
+  let current_time = new Date();
+  console.log(current_time);
+ 
 
-setInterval (function(){
-let current_time=new Date().getTime();
-console.log(current_time);
-
-
-var time_left=new_date-current_time;
-console.log(time_left);
-
-var days = Math.floor(time_left / 3600)/24;
-var hours = Math.floor(time_left /3600)%24;
-var minutes = Math.floor(time_left /60)%60;
-var seconds = Math.floor(time_left %60);
-
-document.getElementById('time1').innerHTML=days +" "+hours+" "+minutes+" "+seconds;
-
-},1000)
-
+  let time_left = new_date - current_time;
+  console.log(time_left);
+  let days = Math.floor(time_left / (1000*60*60*24));
+  let hours = Math.floor(time_left / 3600) % 24;
+  let minutes = Math.floor(time_left / 60) % 60;
+  let seconds = Math.floor(time_left % 60);
+  count.innerHTML=`${days},${hours},${minutes},${seconds}`
+  
+}, 1000);
