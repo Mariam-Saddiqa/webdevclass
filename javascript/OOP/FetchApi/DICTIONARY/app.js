8835c48ef85c92c79bc09098d0eeeb8929f2da3a
Mariam-Saddiqa <mariam_saddiqa@yahoo.com>
2021-08-06 15:49:59 +0200

    update

---
+++ javascript/OOP/FetchApi/DICTIONARY/app.js
@@ -0,0 +1,39 @@
+//API SERVICE: https://dictionaryapi.dev/ > no need api key
+// Example request : https://api.dictionaryapi.dev/api/v2/entries/en/hello
+
+
+const button=document.querySelector('button')
+
+
+button.addEventListener('click',getMeaning)
+const audio=document.querySelector('#play-pause-button')
+
+ 
+
+function getMeaning(){
+
+const input=document.querySelector('#search').value
+const api_key="https://api.dictionaryapi.dev/api/v2/entries/en/"+input+"";
+
+
+fetch(api_key)
+.then(res=>res.json())
+.then(data=>{
+    console.log(data)
+    document.querySelector('h3').innerHTML=input;
+    document.querySelector('.pronunciation-text').innerHTML=data[0].phonetics[0].text;
+    document.querySelector('.definition').children[0].innerHTML=data[0].meanings[0].definitions[0].definition;
+    document.querySelector('.example').innerHTML=data[0].meanings[0].definitions[0].example
+    let mp3_link=data[0].phonetics[0].audio
+    console.log(audio)
+    audio.addEventListener('click',data=>{
+        
+        var audio = new Audio(mp3_link);
+        audio.play();
+    })
+})
+
+
+}
+
+
